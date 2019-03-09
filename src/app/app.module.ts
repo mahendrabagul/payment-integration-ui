7f1aed8516be7dc406c9ce2c49b8294aae308455
Mahendra Bagul <mahendrahiraman.bagul@team.telstra.com>
2019-03-09 17:24:21 +0530

    Added only amount

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentComponent } from './payment/payment.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, FormsModule,
+    AppRoutingModule, HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: PaymentComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/payment/payment.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.css']
+})
+export class PaymentComponent implements OnInit {
+  public payuform: any = {
+    email: 'bagulm123@gmail.com',
+    name: 'Mahendra Bagul',
+    phone: '+91 8484947814',
+    productinfo: 'Google Home Mini'
+  };
+  disablePaymentButton = true;
+  constructor(private http: HttpClient) { }
+
+  confirmPayment() {
+    const paymentPayload = {
+      email: this.payuform.email,
+      name: this.payuform.name,
+      phone: this.payuform.phone,
+      productinfo: this.payuform.productinfo,
+      amount: this.payuform.amount
+    };
+    return this.http.post<any>('http://localhost:8070'
+      + '/v1/payment/gateway/payumoney/payment-details', paymentPayload).subscribe(
+        data => {
+          this.payuform.txnid = data.txnid;
+          this.payuform.surl = data.surl;
+          this.payuform.furl = data.furl;
+          this.payuform.key = data.key;
+          this.payuform.hash = data.hash;
+          this.disablePaymentButton = false;
+        }, error1 => {
+          console.log(error1);
+        });
+  }
+
+  ngOnInit() {
+  }
+}
+
